27f55a85994e3ec87f66ce692e206791120598d1
Adam <a.szummer@gmail.com>
2019-02-24 19:09:14 +0100

    registration

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from app import app
+from app import app, db
 from app.models import User
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm,RegistrationFrom
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
@@ -38,9 +38,24 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
+    return render_template('login.html', title='Log In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationFrom()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congrats, you are now registerd user!")
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
