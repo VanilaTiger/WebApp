c88a5a832e376dba051c1fa7c280e3fded20bb08
VanilaTiger <a.szummer@gmail.com>
2019-03-05 18:50:20 +0100

    Functioning well MailTest module

--- MailTest.py
+++ MailTest.py
@@ -1,19 +1,29 @@
-from flask_mail import Message
-from app import mail, app
-import os
-
-MAIL_SERVER = os.environ.get('smtp.gmail.com')
-MAIL_PORT = int(os.environ.get("587") or 25)
-MAIL_USE_TLS = os.environ.get("True") is not None
-MAIL_USERNAME = os.environ.get('flaskemailcognibe@gmail.com')
-MAIL_PASSWORD = os.environ.get('Flask1!!!')
-ADMINS = ['flaskemailcognibe@gmail.com']
-
-msg = Message('test subject', sender=app.config['ADMINS'][0],
-recipients=['flaskemailcognibe@gmail.com'])
-msg.body = 'text body'
-msg.html = '<h1>HTML body</h1>'
-mail.send(msg)
-
 #Flask1!!!
 #flaskemailcognibe@gmail.com
+
+#działajacy
+
+import smtplib, ssl
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+sender_email = "flaskemailcognibe@gmail.com"
+password = "Flask1!!!"
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(smtp_server,port)
+    server.ehlo() # Can be omitted
+    server.starttls(context=context) # Secure the connection
+    server.ehlo() # Can be omitted
+    server.login(sender_email, password)
+    # TODO: Send email here
+    server.sendmail(sender_email, "madar89@gmail.com", "Python email")
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
